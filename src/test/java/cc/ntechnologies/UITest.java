d2c4278ab7695c9f8125467297caed892a962665
Aleksandr Mikheev <qqalexqq@yandex.ru>
2015-11-30 11:07:56 +0100

    Added deployment/development configurations.

---
+++ src/test/java/cc/ntechnologies/UITest.java
@@ -0,0 +1,16 @@
+package cc.ntechnologies;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import static org.junit.Assert.assertTrue;
+
+
+public class UITest {
+
+    private static final String BASE_URL = "http://localhost:8080";
+    private WebDriver driver = new HtmlUnitDriver();
+
+}
