fa27c5ae774a892e54ecc2246352c29eb5dcb3dc
Aleksandr Mikheev <qqalexqq@yandex.ru>
2015-12-04 11:38:36 +0100

    Fixed GAE-related problems with execution.

--- src/main/java/cc/ntechnologies/Bootstrap.java
+++ src/main/java/cc/ntechnologies/Bootstrap.java
@@ -1,24 +1,15 @@
 package cc.ntechnologies;
 
 import com.googlecode.objectify.ObjectifyService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import cc.ntechnologies.entities.*;
 
 public class Bootstrap {
 
-    private static final Logger log = LoggerFactory.getLogger(Bootstrap.class);
-
     protected void init() {
-        log.info("Starting app");
-
         registerEntities();
     }
 
     private void registerEntities() {
-        log.info("Registering entities...");
-
         ObjectifyService.register(Conference.class);
         ObjectifyService.register(News.class);
         ObjectifyService.register(Organizer.class);
@@ -26,7 +17,5 @@ public class Bootstrap {
         ObjectifyService.register(Room.class);
         ObjectifyService.register(Speaker.class);
         ObjectifyService.register(User.class);
-
-        log.info("Done registering entities.");
     }
 }
