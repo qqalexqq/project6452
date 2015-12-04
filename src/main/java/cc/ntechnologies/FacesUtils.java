2e02fb53e25890d0606bda8333da304c61a98398
Aleksandr Mikheev <qqalexqq@yandex.ru>
2015-12-04 10:59:26 +0100

    Added utility methods for JSF 2

--- src/main/java/cc/ntechnologies/FacesUtils.java
+++ src/main/java/cc/ntechnologies/FacesUtils.java
@@ -1,7 +1,24 @@
 package cc.ntechnologies;
 
-/**
- * Created by aleksandr on 03.12.15.
- */
-public class FacesUtils {
+import org.springframework.stereotype.Component;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import java.io.Serializable;
+
+@Component
+public class FacesUtils implements Serializable {
+    public void addSuccessMessage(String msg) {
+        addMessage(FacesMessage.SEVERITY_INFO, msg);
+    }
+
+    public void addErrorMessage(String msg) {
+        addMessage(FacesMessage.SEVERITY_ERROR, msg);
+    }
+
+    private void addMessage(FacesMessage.Severity severity, String msg) {
+        final FacesMessage facesMsg = new FacesMessage(severity, msg, msg);
+        FacesContext.getCurrentInstance().addMessage(null, facesMsg);
+    }
 }
+
