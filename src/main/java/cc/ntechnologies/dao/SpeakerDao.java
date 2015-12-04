587fa261126a862ca7c68fe43a172e000f6e71df
Aleksandr Mikheev <qqalexqq@yandex.ru>
2015-12-04 11:32:17 +0100

    Added simple DAO to the Speaker class.

--- src/main/java/cc/ntechnologies/dao/SpeakerDao.java
+++ src/main/java/cc/ntechnologies/dao/SpeakerDao.java
@@ -1,7 +1,39 @@
 package cc.ntechnologies.dao;
 
+import com.googlecode.objectify.Key;
+import com.googlecode.objectify.ObjectifyService;
+import cc.ntechnologies.entities.Speaker;
+import org.springframework.stereotype.Repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import static com.googlecode.objectify.ObjectifyService.ofy;
+
 /**
  * Created by aleksandr on 03.12.15.
  */
-public class SpeakerDao {
+@Repository
+public class SpeakerDao implements Serializable {
+
+    private final static long serialVersionUID = 1L;
+
+    public Speaker findById(Long id) {
+        return ofy().load().type(Speaker.class).id(id).now();
+    }
+
+    public void delete(Speaker speaker) {
+        ofy().delete().entity(speaker).now();
+    }
+
+    public Speaker save(Speaker speaker) {
+        ofy().save().entities(speaker).now();
+
+        return speaker;
+    }
+
+    public List<Speaker> getAll() {
+        return ofy().load().type(Speaker.class).list();
+    }
 }
+
