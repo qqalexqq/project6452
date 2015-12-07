dc4ff575f011bd9cee0a71c820676db87f3cc700
Aleksandr Mikheev <qqalexqq@yandex.ru>
2015-12-08 00:33:52 +0100

    Changed API to use dependency injection.

--- src/main/java/cc/ntechnologies/API.java
+++ src/main/java/cc/ntechnologies/API.java
@@ -17,37 +17,43 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
+import javax.inject.Inject;
+
 @Api(name="api", version = "v1", namespace = @ApiNamespace(ownerDomain="project645-2.appspot.com", ownerName="ntech", packagePath=""))
 public class API {
-    private SpeakerDao speakerDao = new SpeakerDao();
-    private ConferenceDao conferenceDao = new ConferenceDao();
-    private NewsDao newsDao = new NewsDao();
-    private OrganizerDao organizerDao = new OrganizerDao();
-    private PartnerDao partnerDao = new PartnerDao();
-
+    @Inject
     @ApiMethod(name = "getAllSpeakers")
-    public List<Speaker> allSpeakers() {
+    public List<Speaker> allSpeakers(SpeakerDao speakerDao) {
         return speakerDao.getAll();
     }
+
+    @Inject
     @ApiMethod(name = "getAllConferences")
-    public List<Conference> allConferences() {
+    public List<Conference> allConferences(ConferenceDao conferenceDao) {
         return conferenceDao.getAll();
     }
+
+    @Inject
     @ApiMethod(name = "getAllOrganizers")
-    public List<Organizer> allOrganizers() {
+    public List<Organizer> allOrganizers(OrganizerDao organizerDao) {
         return organizerDao.getAll();
     }
+
+    @Inject
     @ApiMethod(name = "getAllPartners")
-    public List<Partner> allPartners() {
+    public List<Partner> allPartners(PartnerDao partnerDao) {
         return partnerDao.getAll();
     }
+
+    @Inject
     @ApiMethod(name = "getAllNews")
-    public List<News> allNews() {
+    public List<News> allNews(NewsDao newsDao) {
         return newsDao.getAll();
     }
 
+    @Inject
     @ApiMethod(name="insertTestSpeaker")
-    public Speaker insertTestSpeaker() {
+    public Speaker insertTestSpeaker(SpeakerDao speakerDao) {
         return speakerDao.save(new Speaker("John", "Smith", "Some of those speaking guys we have."));
     }
 }
