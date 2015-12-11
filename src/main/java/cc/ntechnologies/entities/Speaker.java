88e3689cc7ea2745aedf969b3d9ff946e657bfef
Aleksandr Mikheev <qqalexqq@yandex.ru>
2015-12-11 01:50:14 +0100

    Speaker's getters/setters for primefaces datatable

--- src/main/java/cc/ntechnologies/entities/Speaker.java
+++ src/main/java/cc/ntechnologies/entities/Speaker.java
@@ -2,12 +2,20 @@ package cc.ntechnologies.entities;
 
 import com.googlecode.objectify.annotation.*;
 
+import java.io.Serializable;
+
 @Entity
-public class Speaker {
-	@Id public Long id;
-	@Index public String firstname;
-	@Index public String lastname;
-	@Index public String description;
+public class Speaker implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Long id;
+	@Index
+	private String firstname;
+	@Index
+	private String lastname;
+	@Index
+	private String description;
 	// PHOTO
 
 	public Speaker () {
@@ -15,8 +23,35 @@ public class Speaker {
 	}
 
 	public Speaker(String firstname, String lastname, String description){
+		this.setFirstname(firstname);
+		this.setLastname(lastname);
+		this.setDescription(description);
+	}
+
+	public String getFullName() { return String.format("%s %s", this.getFirstname(), this.getLastname()); }
+
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
 		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
 		this.lastname = lastname;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
 		this.description = description;
 	}
 }
