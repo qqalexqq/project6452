ceabedf8ef49d167c50646ebb4412a29eb06e940
Aleksandr Mikheev <qqalexqq@yandex.ru>
2016-01-05 22:39:03 +0300

    Moved API from DAO to using objectify methods.

--- src/main/java/cc/ntechnologies/API.java
+++ src/main/java/cc/ntechnologies/API.java
@@ -2,11 +2,6 @@ package cc.ntechnologies;
 
 import java.util.List;
 
-import cc.ntechnologies.dao.ConferenceDao;
-import cc.ntechnologies.dao.NewsDao;
-import cc.ntechnologies.dao.OrganizerDao;
-import cc.ntechnologies.dao.PartnerDao;
-import cc.ntechnologies.dao.SpeakerDao;
 import cc.ntechnologies.entities.Conference;
 import cc.ntechnologies.entities.News;
 import cc.ntechnologies.entities.Organizer;
@@ -17,37 +12,33 @@ import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
 
-import javax.inject.Inject;
+import static com.googlecode.objectify.ObjectifyService.ofy;
 
-@Api(name="api", version = "v1", namespace = @ApiNamespace(ownerDomain="project645-2.appspot.com", ownerName="ntech", packagePath=""))
+
+@Api(name="api", version = "1.00", namespace = @ApiNamespace(ownerDomain="project645-2.appspot.com", ownerName="ntech", packagePath=""))
 public class API {
-    @Inject
-    @ApiMethod(name = "getAllSpeakers")
-    public List<Speaker> allSpeakers(SpeakerDao speakerDao) {
-        return speakerDao.getAll();
+    @ApiMethod(name = "getAllSpeakers", httpMethod = "GET")
+    public List<Speaker> allSpeakers() {
+        return ofy().load().type(Speaker.class).list();
     }
 
-    @Inject
-    @ApiMethod(name = "getAllConferences")
-    public List<Conference> allConferences(ConferenceDao conferenceDao) {
-        return conferenceDao.getAll();
+    @ApiMethod(name = "getAllConferences", httpMethod = "GET")
+    public List<Conference> allConferences() {
+        return ofy().load().type(Conference.class).list();
     }
 
-    @Inject
-    @ApiMethod(name = "getAllOrganizers")
-    public List<Organizer> allOrganizers(OrganizerDao organizerDao) {
-        return organizerDao.getAll();
+    @ApiMethod(name = "getAllOrganizers", httpMethod = "GET")
+    public List<Organizer> allOrganizers() {
+        return ofy().load().type(Organizer.class).list();
     }
 
-    @Inject
-    @ApiMethod(name = "getAllPartners")
-    public List<Partner> allPartners(PartnerDao partnerDao) {
-        return partnerDao.getAll();
+    @ApiMethod(name = "getAllPartners", httpMethod = "GET")
+    public List<Partner> allPartners() {
+        return ofy().load().type(Partner.class).list();
     }
 
-    @Inject
-    @ApiMethod(name = "getAllNews")
-    public List<News> allNews(NewsDao newsDao) {
-        return newsDao.getAll();
+    @ApiMethod(name = "getAllNews", httpMethod = "GET")
+    public List<News> allNews() {
+        return ofy().load().type(News.class).list();
     }
 }
